a71d8c14b11b40f7bcb5982c34610bb04de5bb47
Frederico Alves <fredericoalves.info@gmail.com>
2022-10-07 10:23:28 +0200

    sourfreaks with vite

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,23 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 export function Home() {
-    return <h1>Home</h1>
+    return (
+        <>
+        <div>
+            <h1>Home</h1>
+        </div>
+
+        <Button>
+            <Link to="/store">Go to Store</Link>
+        </Button>
+
+
+        </>
+
+
+
+
+
+    )
 }
